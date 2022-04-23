4d5e3a6a95dc3eb266b5edcab56012f387bfd6e4
Wonsang Kim <rosskysoss@gmail.com>
2022-04-23 16:22:59 +0900

    null checker debugging

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -5,7 +5,7 @@ const webBackgroundColor = Color.fromRGBO(18, 18, 18, 1);
 const mobileSearchColor = Color.fromRGBO(38, 38, 38, 1);
 const blueColor = Color.fromARGB(255, 0, 180, 246);
 // const primaryColor = Color.fromARGB(255, 0, 0, 255);
-const primaryColor = Colors.green;
+const primaryColor = Color.fromRGBO(45, 152, 104, 1);
 
 const secondaryColor = Colors.grey;
 const whiteColor = Colors.white;
