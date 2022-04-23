cdaf85bb2abe71c836bcad1c0d8f5d1426193946
Wonsang Kim <rosskysoss@gmail.com>
2022-04-23 13:35:52 +0900

    23rd satureday gathering

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -228,13 +228,124 @@ class _MainPageState extends State<MainPage>
                                   },
                                   child: Container(
                                     width: double.infinity,
-                                    height: 200,
-                                    child: Image.network(
-                                      snapshot.data!.docs[index]
-                                          .data()['thumbnailUrl'],
+                                    height: 140,
+                                    child: Row(
+                                      children: [
+                                        Stack(
+                                          children: [
+                                            Container(
+                                              height: 120,
+                                              width: 120,
+                                              decoration: BoxDecoration(
+                                                  borderRadius:
+                                                      BorderRadius.circular(16),
+                                                  border: Border.all(
+                                                      color: Colors.white)),
+                                              child: ClipRRect(
+                                                borderRadius:
+                                                    BorderRadius.circular(16),
+                                                child: SizedBox.fromSize(
+                                                  child: Image.network(
+                                                    snapshot.data!.docs[index]
+                                                        .data()['thumbnailUrl'],
+                                                    fit: BoxFit.cover,
+                                                  ),
+                                                ),
+                                              ),
+                                            ),
+                                            Positioned(
+                                              child: Icon(Icons
+                                                  .bookmark_border_outlined),
+                                              bottom: 8,
+                                              right: 8,
+                                            )
+                                          ],
+                                        ),
+                                        SizedBox(
+                                          width: 20,
+                                        ),
+                                        Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            SizedBox(
+                                              height: 8,
+                                            ),
+                                            Text(
+                                              snapshot.data!.docs[index]
+                                                  .data()['title'],
+                                              // snapshot.data!.docs[index]
+                                              //     .data()['snapId'],
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 18),
+                                            ),
+                                            Text(
+                                              snapshot.data!.docs[index]
+                                                  .data()['description']
+                                                  .substring(0, 30),
+                                              style:
+                                                  TextStyle(color: Colors.grey),
+                                            ),
+                                            SizedBox(
+                                              height: 6,
+                                            ),
+                                            // RichText(
+                                            //     text: TextSpan(
+                                            //         children: <TextSpan>[
+                                            //       TextSpan(
+                                            //         text: '20% ',
+                                            //         style: TextStyle(
+                                            //             color: Colors.red,
+                                            //             fontWeight:
+                                            //                 FontWeight.bold,
+                                            //             fontSize: 24),
+                                            //       ),
+                                            //       TextSpan(
+                                            //         text: '30,000',
+                                            //         style: TextStyle(
+                                            //             color: whiteColor,
+                                            //             fontWeight:
+                                            //                 FontWeight.bold,
+                                            //             fontSize: 24),
+                                            //       ),
+                                            //     ])),
+                                            // SizedBox(
+                                            //   height: 8,
+                                            // ),
+                                            Row(
+                                              children: [
+                                                Chip(
+                                                  backgroundColor: Colors
+                                                      .green.shade900
+                                                      .withOpacity(0.3),
+                                                  label: Text(
+                                                    '#JavaScript',
+                                                    style: TextStyle(
+                                                        fontSize: 12,
+                                                        color: Colors.green),
+                                                  ),
+                                                ),
+                                                SizedBox(width: 6),
+                                                Chip(
+                                                  backgroundColor: Colors
+                                                      .green.shade900
+                                                      .withOpacity(0.3),
+                                                  label: Text(
+                                                    '#마켓컬리',
+                                                    style: TextStyle(
+                                                        fontSize: 12,
+                                                        color: Colors.green),
+                                                  ),
+                                                ),
+                                              ],
+                                            )
+                                          ],
+                                        )
+                                      ],
                                     ),
                                   ),
-                                ),
+                                )
                               ],
                             );
                           },
--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -4,6 +4,8 @@ const mobileBackgroundColor = Color.fromRGBO(0, 0, 0, 1);
 const webBackgroundColor = Color.fromRGBO(18, 18, 18, 1);
 const mobileSearchColor = Color.fromRGBO(38, 38, 38, 1);
 const blueColor = Color.fromARGB(255, 0, 180, 246);
-const primaryColor = Color.fromARGB(255, 0, 0, 255);
+// const primaryColor = Color.fromARGB(255, 0, 0, 255);
+const primaryColor = Colors.green;
+
 const secondaryColor = Colors.grey;
 const whiteColor = Colors.white;
